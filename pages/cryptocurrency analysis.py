8152618caf1b160d00d5f2bf919bc2e976b7b086
Shalinipal19 <shalupal804@gmail.com>
2023-07-28 10:31:20 +0530

    minor project

--- pages/cryptocurrency analysis.py
+++ pages/cryptocurrency analysis.py
@@ -17,38 +17,26 @@ if st.sidebar.checkbox("view raw data"):
     st.subheader(f'📅{cryptocurrencies} data')
     st.dataframe(df, use_container_width=True)
 
-df['Date'] = pd.to_datetime(df['Date'])
-df.set_index('Date', inplace=True)
-
-period = st.radio('Select time period sample', ['D','W',"M",'Y'], horizontal=True)
-
-st.subheader("Cryptocurrencies openining")
-dfopen = df.resample(period)['Open'].sum()
-fig1 = px.line(dfopen, dfopen.index, 'Open', title="cryptocurrencies graph for opening")
+st.subheader("Cryptocurrencies opening")
+fig1 = px.pie(df, 'Year', 'Open', title="cryptocurrencies graph for opening")
 st.plotly_chart(fig1, use_container_width=True)
 
 st.subheader("Cryptocurrencies closing")
-dfclose = df.resample(period)['Close'].sum()
-fig1 = px.line(dfclose, dfclose.index, 'Close', title="cryptocurrencies graph for closing")
+fig1 = px.pie(df, 'Year', 'Close', title="cryptocurrencies graph for closing")
 st.plotly_chart(fig1, use_container_width=True)
 
 st.subheader("Cryptocurrencies high")
-dfhigh = df.resample(period)['High'].sum()
-fig1 = px.line(dfhigh, dfhigh.index, 'High', title="cryptocurrencies graph for high")
+fig1 = px.pie(df, 'Year', 'High', title="cryptocurrencies graph for high")
 st.plotly_chart(fig1, use_container_width=True)
 
 st.subheader("Cryptocurrencies low")
-dflow = df.resample(period)['Low'].sum()
-fig1 = px.line(dflow, dflow.index, 'Low', title="cryptocurrencies graph for low")
+fig1 = px.pie(df, 'Year', 'Low', title="cryptocurrencies graph for low")
 st.plotly_chart(fig1, use_container_width=True)
 
 st.subheader("Cryptocurrencies ytd gains")
-dfytdgains = df.resample(period)['YTD Gains'].sum()
-fig1 = px.line(dfytdgains, dfytdgains.index, 'YTD Gains', title="cryptocurrencies graph for ytd gains")
+fig1 = px.line(df,'year','YTD Gains', title="cryptocurrencies graph for ytd gains")
 st.plotly_chart(fig1, use_container_width=True)
 
 st.subheader("Cryptocurrencies volume")
-dfvolume = df.resample(period)['Volume'].sum()
-fig1 = px.line(dfvolume, dfvolume.index, 'Volume', title="cryptocurrencies graph for volume")
+fig1 = px.line(df,'Year','Volume', title="cryptocurrencies graph for volume")
 st.plotly_chart(fig1, use_container_width=True)
-
--- pages/currency analysis.py
+++ pages/currency analysis.py
@@ -15,41 +15,30 @@ currencies = st.sidebar.radio('Select a currencies', files)
 df = pd.read_csv(currencies)
 if st.sidebar.checkbox("view raw data"):
     st.subheader(f'📅{currencies} data')
-    st.dataframe(df)
+    st.dataframe(df, use_container_width=True)
 
-df['Date'] = pd.to_datetime(df['Date'])
-df.set_index('Date', inplace=True)
-
-period = st.radio('Select time period sample', ['D','W',"M",'Y'], horizontal=True)
-
-st.subheader("Currencies openining")
-dfopen = df.resample(period)['Open'].sum()
-fig1 = px.line(dfopen, dfopen.index, 'Open', title="currencies graph for opening")
+st.subheader("Currencies opening")
+fig1 = px.pie(df, 'Year', 'Open', title="currencies graph for opening")
 st.plotly_chart(fig1, use_container_width=True)
 
 st.subheader("Currencies closing")
-dfclose = df.resample(period)['Close'].sum()
-fig1 = px.line(dfclose, dfclose.index, 'Close', title="currencies graph for closing")
+fig1 = px.pie(df, 'Year', 'Close', title="currencies graph for closing")
 st.plotly_chart(fig1, use_container_width=True)
 
 st.subheader("Currencies high")
-dfhigh = df.resample(period)['High'].sum()
-fig1 = px.line(dfhigh, dfhigh.index, 'High', title="currencies graph for high")
+fig1 = px.bar(df, 'Year', 'High', title="currencies graph for high")
 st.plotly_chart(fig1, use_container_width=True)
 
 st.subheader("Currencies low")
-dflow = df.resample(period)['Low'].sum()
-fig1 = px.line(dflow, dflow.index, 'Low', title="currencies graph for low")
+fig1 = px.bar(df, 'Year', 'Low', title="currencies graph for low")
 st.plotly_chart(fig1, use_container_width=True)
 
 st.subheader("Currencies ytd gains")
-dfytdgains = df.resample(period)['YTD Gains'].sum()
-fig1 = px.line(dfytdgains, dfytdgains.index, 'YTD Gains', title="currencies graph for ytd gains")
+fig1 = px.line(df,'year','YTD Gains', title="currencies graph for ytd gains")
 st.plotly_chart(fig1, use_container_width=True)
 
 st.subheader("Currencies volume")
-dfvolume = df.resample(period)['Volume'].sum()
-fig1 = px.line(dfvolume, dfvolume.index, 'Volume', title="currencies graph for volume")
+fig1 = px.line(df,'Year','Volume', title="currencies graph for volume")
 st.plotly_chart(fig1, use_container_width=True)
 
 
